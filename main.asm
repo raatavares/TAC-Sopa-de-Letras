43cc81fb6581ef0c24709632aa543813a943416a
João Mota <102801763+roooogers@users.noreply.github.com>
2022-06-07 17:03:53 +0100

    Ficheiros

--- main.asm
+++ main.asm
@@ -56,3 +56,29 @@ dseg	segment para public 'data'
 				
 	Bem_Vindo	db "Bem-vindo ao jogo",13,10
 				db "Prima qualquer tecla para continuar...",13,10,'$'
+				
+				
+				
+				
+
+
+    ; CONTADOR
+	
+	timer			dw 	    0				; Contador de tempo
+	Horas			dw		0				; Guarda a hora atual
+	Minutos			dw		0				; Guarda os minutos actuais
+	Segundos		dw		0				; Guarda os segundos actuais
+	Old_seg			dw		0				; Guarda os ultimos segundos que foram lidos
+	Tempo_init		dw		0				; Guarda o tempo de inicio do jogo
+	Tempo_j			dw		-1				; Guarda o tempo que decorre o jogo
+	Tempo_limite	dw		100				; Tempo maximo de Jogo
+	String_TJ		db		"     / 100$"
+	String_TJ		db		"     / 100$"
+	
+	
+	; FICHEIROS
+	
+	Erro_Open       db      'Erro ao tentar iniciar o jogo$'
+	FichTabela             db      'PAINEL.txt',0
+	FichTop10           db      'TOP10.txt',0
+	
